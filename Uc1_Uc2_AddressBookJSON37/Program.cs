87e0aaaa18c6a9d342db6acd50f16ed0ec71699c
Harry <hareshwarp649>
2022-05-21 09:53:44 +0530

    [HRPatil]Added:Add multiple Entries to AddBook JsonServer

---
+++ Uc1_Uc2_AddressBookJSON37/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uc1_Uc2_AddressBookJSON37
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WELCOME AddressBook JSON Server");
+        }
+    }
+}
