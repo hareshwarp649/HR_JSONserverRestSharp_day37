d892f984c629d6ddbe1c35d46b5c6db6a9227f57
Harry <hareshwarp649>
2022-05-21 09:29:47 +0530

    [HRPatil]Added:Add Update Delete Employee using Json Server/RestSharpTest

---
+++ Uc1-37EmpPayRoll_JSON/DataEmpPay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uc1_37EmpPayRoll_JSON
+{
+    public class DataEmpPay
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string salary { get; set; }
+    }
+}
+}
---
+++ Uc1-37EmpPayRoll_JSON/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uc1_37EmpPayRoll_JSON
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Json server");
+        }
+    }
+}
