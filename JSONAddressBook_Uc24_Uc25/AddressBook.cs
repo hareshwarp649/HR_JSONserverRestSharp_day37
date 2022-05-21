abe9fb8fbfee2a05e56036c9d1fbf4cb9a752ef3
Harry <hareshwarp649>
2022-05-21 17:02:41 +0530

    [HRPatil]Added:Ability to Update delete Entry in Address Book JSON Server day37

---
+++ JSONAddressBook_Uc24_Uc25/AddressBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONAddressBook_Uc24_Uc25
+{
+    public class AddressBook
+    {
+        public int id { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string phoneNumber { get; set; }
+        public string address { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string zip { get; set; }
+        public string email { get; set; }
+    }
+}
